1a94f8e2cad63d40b41c2e0e0b7a56b8dba459a3
Stella Silvaroli <99274582+Stella-Silvaroli06@users.noreply.github.com>
2022-03-24 13:39:27 +0000

    Added and edited

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2022 Ms Raffin All rights reserved
 //
-// Created by: Ms Raffin
-// Created on: Sep 2022
+// Created by: Stella S
+// Created on: Mar 2022
 // This file contains the JS functions for index.html
 
 function enterClicked () {
